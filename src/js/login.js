8d9ad8aa91a732977a27f9fab223a80e0de8de32
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2023-06-18 23:31:58 -0300

    Inseridos comentários no código #40

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,7 @@
 function buscaUsuario(nome, senha){
     const jt = localStorage.getItem(nome);
     
-  //se jt não é igual a null, buscar os dados
+  //se jt não é igual a null(não estiver vazio), buscar os dados presentes no local storage. Se não, mostrar mensagem de "Seus dados estão incorretos, tente novamente"
     if(jt!=null){ 
         let pessoa = JSON.parse(jt); 
         if (document.getElementById("usuario").value === pessoa.nome && senha === pessoa.senha) {
@@ -15,6 +15,8 @@ function buscaUsuario(nome, senha){
            window.alert("Seus dados estão incorretos, tente novamente");
         }
     }
+
+    // Caso o jt esteja vazio mostrar mensagem abaixo
     if(jt=null){
       alert("Insira seus dados para continuar!")
     } 

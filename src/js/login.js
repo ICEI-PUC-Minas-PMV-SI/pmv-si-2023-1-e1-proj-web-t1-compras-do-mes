f749e5ab03996353794c0fffb3965b08d3ed3c8a
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2023-06-19 18:23:07 -0300

    Atualizado algumas redundâncias no código JS #40

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,7 @@
 function buscaUsuario(nome, senha){
     const jt = localStorage.getItem(nome);
     
-  //se jt não é igual a null(não estiver vazio), buscar os dados presentes no local storage. Se não, mostrar mensagem de "Seus dados estão incorretos, tente novamente"
+//se jt não é igual a null(não estiver vazio), buscar os dados presentes no local storage. Se não, mostrar mensagem de "Seus dados estão incorretos, tente novamente"
     if(jt!=null){ 
         let pessoa = JSON.parse(jt); 
         if (document.getElementById("usuario").value === pessoa.nome && senha === pessoa.senha) {
@@ -12,12 +12,13 @@ function buscaUsuario(nome, senha){
             
         }
         else{
-           window.alert("Seus dados estão incorretos, tente novamente");
+          alert("Seus dados estão incorretos, tente novamente");
         }
-      }else if (jt === null) {alert("Insira seus dados para continuar!")}
+// Caso o jt esteja vazio mostrar mensagem abaixo
+      }else {alert("Insira seus dados para continuar!")}
     }
 
-    // Caso o jt esteja vazio mostrar mensagem abaixo
+
     
 
      

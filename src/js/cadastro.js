5e6d317dd624cdc09487a4b40b5e4da89317b889
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2023-06-19 00:41:05 -0300

    Inseridos alguns comentários no cadastro.js #40

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,5 +1,6 @@
 
 function cadastrar(){
+    //a função criará um objeto novo de com as informações de usuário (novousuario.nome) e senha (novousuario.senha) conforme o que for inserido no input.
     if (document.getElementById("senha").value === document.getElementById("confsenha").value) 
     {
     let novousuario = new Object(); 
@@ -11,6 +12,8 @@ function cadastrar(){
     localStorage.setItem(novousuario.nome, jt);
     location.href = "/src/tela-de-login.html";}
     
+    //caso as informações nos campos de "senha" e "confirme senha" não estejam iguais, o código acima não será rodado e a mensagem abaixo aparecerá
+
     else {alert('"Senha" e "Confirme a senha" devem ser o mesmo dado.')}
     
    }

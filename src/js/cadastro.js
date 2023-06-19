e6c1cbe279078903048ee419408eb290e8787ae8
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2023-06-19 00:38:18 -0300

    Atualizado JS de cadastro para que a senha seja validada #40

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,13 +1,16 @@
 
 function cadastrar(){
+    if (document.getElementById("senha").value === document.getElementById("confsenha").value) 
+    {
     let novousuario = new Object(); 
     novousuario.nome=document.getElementById("usuario").value; 
     novousuario.senha=document.getElementById("senha").value; 
     novousuario.confsenha=document.getElementById("confsenha").value; 
-    
     window.alert("Cadastro realizado! Realize seu login!");
     let jt = JSON.stringify(novousuario); 
     localStorage.setItem(novousuario.nome, jt);
-    location.href = "/src/tela-de-login.html";
+    location.href = "/src/tela-de-login.html";}
+    
+    else {alert('"Senha" e "Confirme a senha" devem ser o mesmo dado.')}
     
    }

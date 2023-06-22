912e0b4fbe76bf98af4c23474340d7568e9a50b2
Vitoria Lorrayne Dos Santos Soares <vitoria.lorrayne@sga.pucminas.br>
2023-06-21 22:01:21 -0300

    #40 logout

--- src/js/paginalogada.js
+++ src/js/paginalogada.js
@@ -1,4 +1,15 @@
-function logout() {
+window.onload = function usuarioLogado() {
+    let nomeUsuario = localStorage.getItem('usuario_logado');
+    if(nomeUsuario==null || nomeUsuario=="" || nomeUsuario=="null"){
+       location.href = "/src/calendario-de-lista.html";
+ 
+    }
+    var tagA = document.getElementById("nome");
+    tagA.innerHTML = `${nomeUsuario}`; 
+    
+ }
+ 
+ function logout() {
     window.alert("Você foi desconectado com sucesso! ");
     localStorage.removeItem('usuario_logado');
     location.href = "/src/tela-de-login.html";

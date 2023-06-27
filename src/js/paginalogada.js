4e88352d471b5faa2af549bed74310954e3e8cdf
Vitoria Lorrayne Dos Santos Soares <vitoria.lorrayne@sga.pucminas.br>
2023-06-27 17:41:04 -0300

    #40 corrigindo bug gerado na tela

--- src/js/paginalogada.js
+++ src/js/paginalogada.js
@@ -1,14 +1,4 @@
-window.onload = function usuarioLogado() {
-    let nomeUsuario = localStorage.getItem('usuario_logado');
-    if(nomeUsuario==null || nomeUsuario=="" || nomeUsuario=="null"){
-       location.href = "/src/lista-de-gastos.html";
- 
-    }
-    var tagA = document.getElementById("nome");
-    tagA.innerHTML = `${}`; 
-    
- }
- 
+
  function logout() {
     window.alert("Você foi desconectado com sucesso! ");
     localStorage.removeItem('usuario_logado');

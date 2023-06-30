e73773dfc6361431694f8244bfacdee94cc86db1
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2023-06-30 01:00:02 -0300

    Atualizado usuário em navbar #43

--- src/js/main.js
+++ src/js/main.js
@@ -242,3 +242,12 @@ $(document).ready(function() {
   });
 
 // Gráfico
+
+// Usuário em navbar
+onload = function usuarioLogado() {
+  let nomeUsuario = localStorage.getItem('usuario_logado');
+  
+  var tagA = document.getElementById("nome");
+  tagA.innerHTML = `${nomeUsuario}`; 
+  
+}
